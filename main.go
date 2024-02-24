558f3b7095a3c1040732afdab4d3b83982090900
root <root@bryce>
2024-02-23 19:09:11 -0500

    Exploring Handler Conversions

--- main.go
+++ main.go
@@ -15,24 +15,23 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:bryce.ziemer@gmail.com\">bryce.ziemer@gmail.com</a>.")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
 
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHandler(w, r)
 
-	default:
-		//w.WriteHeader(http.StatusNotFound)
-		//fmt.Fprint(w, "Page Not Found!")
-		http.Error(w, "Page Not Found", http.StatusNotFound)
+// 	default:
+// 		//w.WriteHeader(http.StatusNotFound)
+// 		//fmt.Fprint(w, "Page Not Found!")
+// 		http.Error(w, "Page Not Found", http.StatusNotFound)
 
-	}
+// 	}
 
-}
+// }
 
-/*
 type Router struct{}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -50,11 +49,11 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
-*/
 
 func main() {
-
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	//http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", router)
 
 }
