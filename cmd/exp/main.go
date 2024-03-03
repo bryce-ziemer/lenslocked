84cb4145eeaa8a3799ffe55222ee7f2f2a75a9ae
root <root@bryce>
2024-03-03 10:48:40 -0500

    Wrapped pase call in main with Must function

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,33 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
+func blah() error {
+	panic("...")
+}
+
 func main() {
-	err := CreateOrg()
-	fmt.Println(err)
-
-}
-
-func Connect() error {
-	return errors.New("Connection Failed")
-}
-
-func CreateUser() error {
-	err := Connect()
-	if err != nil {
-		return fmt.Errorf("create user: %w", err)
-	}
-	//... continue on
-	return nil
-}
-
-func CreateOrg() error {
-	err := CreateUser()
-	if err != nil {
-		return fmt.Errorf("create org: %w", err)
-	}
-	return nil
+	numbers := []int{1, 2, 3}
+	fmt.Println(numbers[4])
 }
