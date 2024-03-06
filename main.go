262fbeeb3a7489d6ab8e9f9183d6facf2536bdff
root <root@bryce>
2024-03-06 17:51:33 -0500

    exploring 2 different layout setups

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	tpl := views.Must(views.ParseFS(templates.FS, "home.gohtml"))
+	tpl := views.Must(views.ParseFS(templates.FS, "layout-page.gohtml", "home.gohtml"))
 	r.Get("/", controllers.FAQ(tpl))
 
 	tpl = views.Must(views.ParseFS(templates.FS, "contact.gohtml"))
