4019ea40d6fb4e65dccc73a02d12094e3428b50d
root <root@bryce>
2024-03-15 18:57:52 -0400

    installed pgx and wrote some expiremental demo code for it

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
-func blah() error {
-	panic("...")
-}
-
 func main() {
-	numbers := []int{1, 2, 3}
-	fmt.Println(numbers[4])
+	db, err := sql.Open("pgx", "host=localhost port=5432 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("connected")
 }
