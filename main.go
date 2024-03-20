f51535b83b5e9ee7039398abfe77a85e2abaf619
root <root@bryce>
2024-03-20 18:15:14 -0400

    Added signin page

--- main.go
+++ main.go
@@ -47,8 +47,16 @@ func main() {
 		"signup.gohtml",
 		"tailwind.gohtml",
 	))
+
+	usersC.Templates.SignIn = views.Must(views.ParseFS(
+		templates.FS,
+		"signin.gohtml",
+		"tailwind.gohtml",
+	))
+
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
+	r.Get("/signin", usersC.SignIn)
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page Not Found", http.StatusNotFound)
