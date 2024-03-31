1a192f35e323f2583bfffbc107d53c53bb006c6b
root <root@bryce>
2024-03-31 11:03:36 -0400

    stubbed out methods for SessionService

--- models/session.go
+++ models/session.go
@@ -1,7 +1,32 @@
-package model
+package models
+
+import (
+	"database/sql"
+)
 
 type Session struct {
-	ID        int
-	UserID    int
+	ID     int
+	UserID int
+	// Token is only set when creating new session.  When looking up the session
+	// this will be left empty as we only styore the hash of a session token in our DB
+	// and we cannot reverse it into a raw token.
+	Token     string
 	TokenHash string
+}
+
+type SessionService struct {
+	DB *sql.DB
+}
+
+func (ss *SessionService) Create(userID int) (*Session, error) {
+	//1. Create the session token - we want to handlke this in the service bc, otherwise, a poor token may be p[assed in and used]
+	// TODO: create the session token
+	// TODO: Implement SessionService.Create
+	return nil, nil
+
+}
+
+func (sss *SessionService) User(token string) (*User, error) {
+	// TODO Implement SessionService.User
+	return nil, nil
 }
