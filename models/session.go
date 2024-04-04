6321092afe9a2a665fc7405964d102df9e6158c0
root <root@bryce>
2024-04-04 19:21:52 -0400

    Finished out SessionService.Create by inserting the session into the session table

--- models/session.go
+++ models/session.go
@@ -48,8 +48,16 @@ func (ss *SessionService) Create(userID int) (*Session, error) {
 		Token:     token,
 		TokenHash: ss.hash(token),
 	}
-	// TODO store the session in our DB
 
+	row := ss.DB.QueryRow(`
+	INSERT INTO sessions (user_id, token_hash)
+	VALUES ($1, $2)
+	RETURNING id;`, session.UserID, session.TokenHash)
+
+	err = row.Scan(&session.ID)
+	if err != nil {
+		return nil, fmt.Errorf("Create: %w", err)
+	}
 	return &session, nil
 
 }
