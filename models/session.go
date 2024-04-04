640cd1145f235ca84067bfc8f4b608fb4a5fabce
root <root@bryce>
2024-04-04 18:56:26 -0400

    Refactored - moved session specific logic out of rand package

--- models/session.go
+++ models/session.go
@@ -6,6 +6,11 @@ import (
 	"fmt"
 )
 
+const (
+	// The minimum number of bytes to be used for each session token.
+	MinBytesPerToken = 32
+)
+
 type Session struct {
 	ID     int
 	UserID int
@@ -18,10 +23,17 @@ type Session struct {
 
 type SessionService struct {
 	DB *sql.DB
+	// BytesPerToken is used to determine how many bytes to use when generating each session token.
+	// If this value is not set or is less than the MinBytesPerToken const it will be ignored and MinBytesPerToken will be used.
+	BytesPerToken int
 }
 
 func (ss *SessionService) Create(userID int) (*Session, error) {
-	token, err := rand.SessionToken()
+	bytesPerToken := ss.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+	token, err := rand.String(bytesPerToken)
 
 	if err != nil {
 		return nil, fmt.Errorf("Create: %w", err)
