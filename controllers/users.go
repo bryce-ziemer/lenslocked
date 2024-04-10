2b79f42ad9cda8187ea66e064de23fea9421424f
root <root@bryce>
2024-04-10 16:49:43 -0400

    Added new handler for signing out. This involved creating a processSignout method in the userController, which calls a new deleteCookie method in the cookieController

--- controllers/users.go
+++ controllers/users.go
@@ -107,3 +107,23 @@ func (u Users) CurrentUser(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "Current user: %s\n", user.Email)
 }
+
+func (u Users) ProcessSignOut(w http.ResponseWriter, r *http.Request) {
+	token, err := readCookie(r, CookieSession)
+	if err != nil {
+		http.Redirect(w, r, "/signin", http.StatusFound)
+		return
+	}
+
+	err = u.SessionService.Delete(token)
+	if err != nil {
+		println(err)
+		http.Error(w, "Somethign went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	deleteCookie(w, CookieSession)
+
+	http.Redirect(w, r, "/signin", http.StatusFound)
+
+}
