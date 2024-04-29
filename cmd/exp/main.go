27534a3024aa9852d3c3b4560bbf476bc9feeffe
root <root@bryce>
2024-04-29 19:13:36 -0400

    Added code in exp to show type-assertions

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 )
 
 // Best practice so no one else can accidently collide (more unique, because linked to type)
@@ -17,6 +18,25 @@ const (
 func main() {
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, favoriteColorKey, "blue")
-	value := ctx.Value(favoriteColorKey)
-	fmt.Println(value)
+	value := ctx.Value(favoriteColorKey) // value is of type any
+	// fmt.Println(strings.HasPrefix(value, "b")) // WOULD COMPLAIN - value is of type any, not string
+
+	intValue, ok := value.(int)
+
+	if !ok {
+		fmt.Println("Not an int !")
+	} else {
+		fmt.Println(intValue)
+		fmt.Println(intValue + 4)
+	}
+
+	strValue, ok := value.(string)
+	// fmt.Println(strings.HasPrefix(value, "b")) // WOULD COMPLAIN - value is of type any, not string
+
+	if !ok {
+		fmt.Println("Not an int !")
+	} else {
+		fmt.Println(strValue)
+		fmt.Println(strings.HasPrefix(strValue, "b"))
+	}
 }
