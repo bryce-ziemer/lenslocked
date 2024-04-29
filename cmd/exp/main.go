c8db57ceba89e1ec828f7dc36fa3c371045a68a4
root <root@bryce>
2024-04-29 19:01:03 -0400

    Added code to exp to showcase context with types

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -5,9 +5,18 @@ import (
 	"fmt"
 )
 
+// Best practice so no one else can accidently collide (more unique, because linked to type)
+// should not be exportable, local to package (lower case starting letter)
+type ctxKey string
+
+// also do not export keys (gurantee code within this package can access these keys/types)
+const (
+	favoriteColorKey ctxKey = "favorite-color"
+)
+
 func main() {
 	ctx := context.Background()
-	ctx = context.WithValue(ctx, "favorite-color", "blue")
-	value := ctx.Value("favorite-color")
+	ctx = context.WithValue(ctx, favoriteColorKey, "blue")
+	value := ctx.Value(favoriteColorKey)
 	fmt.Println(value)
 }
