0454c4336d33c646bcedc8a6ccf9f597d1824316
root <root@bryce>
2024-05-01 16:12:05 -0400

    Added expiremental code to try out sending emails

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,31 +1,42 @@
 package main
 
 import (
-	stdctx "context"
 	"fmt"
 
-	"bryce-ziemer/github.com/lenslocked/context"
-	"bryce-ziemer/github.com/lenslocked/models"
+	"github.com/go-mail/mail/v2"
 )
 
-// Best practice so no one else can accidently collide (more unique, because linked to type)
-// should not be exportable, local to package (lower case starting letter)
-type ctxKey string
-
-// also do not export keys (gurantee code within this package can access these keys/types)
+// varies depending on mail service
 const (
-	favoriteColorKey ctxKey = "favorite-color"
+	host     = "sandbox.smtp.mailtrap.io"
+	port     = 2525
+	username = "d2d80aa2bea15a"
+	password = "6d22ab77885635"
 )
 
 func main() {
-	ctx := stdctx.Background()
 
-	user := models.User{
-		Email: "me@email.com",
+	from := "test@lenslocked.com"
+	to := "bryce.ziemer@gmail.com" //"jon@calhoun.io"
+	subject := "This is a test email"
+	plaintext := "This is the body of the email"
+	html := `<h1?Hello there buddy!</h1><p>This is the email</p><p>Hope you enjoy it</p>`
+
+	msg := mail.NewMessage()
+	msg.SetHeader("To", to)
+	msg.SetHeader("From", from)
+	msg.SetHeader("Subject", subject)
+	msg.SetBody("text/plain", plaintext)
+	msg.AddAlternative("text/html", html)
+
+	//msg.WriteTo(os.Stdout)
+
+	dialer := mail.NewDialer(host, port, username, password)
+	err := dialer.DialAndSend(msg)
+	if err != nil {
+		panic(err)
 	}
 
-	ctx = context.WithUser(ctx, &user)
+	fmt.Println("Msg Sent")
 
-	retrivedUser := context.User(ctx)
-	fmt.Println(retrivedUser.Email)
 }
