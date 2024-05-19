120a6e550ba93a83c7fd52b79e087132959604d6
root <root@bryce>
2024-05-19 09:58:39 -0400

    Added functionaility to gallery service to get all images (as Image objects) and added expiremental code to do a sanity check

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,52 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strconv"
-
 	"bryce-ziemer/github.com/lenslocked/models"
-
-	"github.com/joho/godotenv"
+	"fmt"
 )
 
 func main() {
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading.env file")
-	}
-
-	host := os.Getenv("SMTP_HOST")
-	portStr := os.Getenv("SMTP_PORT")
-	port, err := strconv.Atoi(portStr)
-	if err != nil {
-		panic(err)
-	}
-	username := os.Getenv("SMTP_USERNAME")
-	password := os.Getenv("SMTP_PASSWORD")
-
-	email := models.Email{
-		From:      "test@lenslocked.com",
-		To:        "bryce.ziemer@gmail.com",
-		Subject:   "This is a test email",
-		Plaintext: "This is the body of the email",
-		HTML:      `<h1>Hello there buddy!</h1><p>This is the email</p><p>Hope you enjoy it</p>`,
-	}
-
-	es := models.NewEmailService(models.SMTPConfig{
-		Host:     host,
-		Port:     port,
-		Username: username,
-		Password: password,
-	})
-
-	err = es.Send(email)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Msg Sent")
+	gs := models.GalleryService{}
+	fmt.Println(gs.Images(2))
 
 }
